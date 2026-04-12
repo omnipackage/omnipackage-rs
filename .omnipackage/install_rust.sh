4d8f479f69759348cca3b5d118177fc5425fe0f3
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-04-12 09:59:24 +0200

    fix rust installation 3

--- .omnipackage/install_rust.sh
+++ .omnipackage/install_rust.sh
@@ -6,8 +6,6 @@ if cargo --version; then
     exit 0
 fi
 
-export RUSTUP_HOME=/usr/local/rustup
-export CARGO_HOME=/usr/local/cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal
 rm -rf ~/.rustup/tmp ~/.rustup/toolchains/*/share/doc
-ln -s /usr/local/cargo/bin/* /usr/bin/
+ln -s /root/.cargo/bin/* /usr/bin/
