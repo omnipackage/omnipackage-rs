d48ccef868e4970c94d81fcd6cd2854b2589d36b
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-04-12 10:35:52 +0200

    fix rust installation 5

--- .omnipackage/install_rust.sh
+++ .omnipackage/install_rust.sh
@@ -6,9 +6,9 @@ if cargo --version; then
     exit 0
 fi
 
-export RUSTUP_HOME=/opt/rust/rustup
-export CARGO_HOME=/opt/rust/cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable
-mv /opt/rust/cargo/bin/* /usr/bin/
-
-rm -rf ~/.rustup/tmp ~/.rustup/toolchains/*/share/doc
+curl -O https://static.rust-lang.org/dist/rust-1.94.1-x86_64-unknown-linux-gnu.tar.gz
+tar -xzf rust-*.tar.gz
+cd rust-*/
+./install.sh --prefix=/usr
+cd ..
+rm -rf rust-*
