b59046e74f3020bee2fb2c6c2578c6e885e80c0b
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-04-12 09:37:11 +0200

    fix rust installation 2

--- .omnipackage/install_rust.sh
+++ .omnipackage/install_rust.sh
@@ -8,6 +8,6 @@ fi
 
 export RUSTUP_HOME=/usr/local/rustup
 export CARGO_HOME=/usr/local/cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable
 rm -rf ~/.rustup/tmp ~/.rustup/toolchains/*/share/doc
 ln -s /usr/local/cargo/bin/* /usr/bin/
