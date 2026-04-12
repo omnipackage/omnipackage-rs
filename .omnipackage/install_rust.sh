8217b30445e5b97c70c892151227c57bff307a49
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-04-12 10:56:15 +0200

    improve rust installation 6

--- .omnipackage/install_rust.sh
+++ .omnipackage/install_rust.sh
@@ -6,7 +6,10 @@ if cargo --version; then
     exit 0
 fi
 
-curl -O https://static.rust-lang.org/dist/rust-1.94.1-x86_64-unknown-linux-gnu.tar.gz
+RUST_VERSION=1.94.1
+ARCH=$(uname -m)
+
+curl -O https://static.rust-lang.org/dist/rust-${RUST_VERSION}-${ARCH}-unknown-linux-gnu.tar.gz
 tar -xzf rust-*.tar.gz
 cd rust-*/
 ./install.sh --prefix=/usr
