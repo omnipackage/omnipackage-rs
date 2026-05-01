eb95247b81db23ff570e15ed5b7c203838472d67
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2026-05-01 23:28:00 +0200

    up rust version

--- .omnipackage/install_rust.sh
+++ .omnipackage/install_rust.sh
@@ -6,7 +6,7 @@ if cargo --version; then
     exit 0
 fi
 
-RUST_VERSION=1.94.1
+RUST_VERSION=1.95.0
 ARCH=$(uname -m)
 
 curl -O https://static.rust-lang.org/dist/rust-${RUST_VERSION}-${ARCH}-unknown-linux-gnu.tar.gz
